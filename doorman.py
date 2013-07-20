cd6cff1dfcc67ea26bcdbb3800da318534d42f25
Sam Kimbrel <skimbrel@twilio.com>
2013-07-19 18:07:42 -0700

    Basic app that just plays 9 to open the door

---
+++ doorman.py
@@ -0,0 +1,15 @@
+import flask
+from twilio import twiml
+
+
+OPEN_DOOR_WAV = 'https://dl.dropboxusercontent.com/u/191231/9.wav'
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def open_door():
+    response = twiml.Response()
+    response.play(OPEN_DOOR_WAV)
+    response.hangup()
+    return response.toxml()
