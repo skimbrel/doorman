4a6a17873af5e1524fb9730dd82e7f0ec3cf8630
Sam Kimbrel <skimbrel@twilio.com>
2013-07-31 18:29:21 -0700

    fucking redistogo only runs 2.4? seriously?!

--- doorman.py
+++ doorman.py
@@ -8,21 +8,24 @@ from twilio import twiml
 OPEN_DOOR_WAV = 'https://dl.dropboxusercontent.com/u/191231/9.wav'
 
 app = flask.Flask(__name__)
-redis_client = redis.Redis(os.environ['REDIS_URL'])
+app.debug = True
+redis_client = redis.Redis.from_url(os.environ['REDIS_URL'])
+print redis_client
 SECRET_KEY = u'1234'
 DOOR_IS_OPEN = 'door-open'
 TIMEOUT = 60  # seconds
 
 
-@app.route('/knock')
+@app.route('/knock', methods=['POST'])
 def knock():
     body = request.form['Body']
     response = twiml.Response()
     if body == SECRET_KEY:
+        redis_client.set(DOOR_IS_OPEN, 'true')
+        redis_client.expire(DOOR_IS_OPEN, 60)
         response.sms(
             u'Passcode accepted. Please dial from the box in the next minute.'
         )
-        redis_client.set(DOOR_IS_OPEN, 'true', ex=TIMEOUT)
     else:
         response.sms(u'Passcode incorrect! Please try again.')
 
@@ -38,3 +41,8 @@ def open_door():
         response.say(u'Sorry, the door is locked.', voice='alice')
     response.hangup()
     return unicode(response)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
