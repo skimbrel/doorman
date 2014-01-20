09f38b348a86a33b3fa4c46c91d3d776a8dd6c22
Sam Kimbrel <samkimbrel@gmail.com>
2014-01-19 18:06:09 -0800

    Accept passcode via Gather if we didn't get it texted to us.

--- doorman.py
+++ doorman.py
@@ -12,6 +12,7 @@ redis_client = redis.Redis.from_url(os.environ['REDIS_URL'])
 SECRET_KEY = u'1234'
 DOOR_IS_OPEN = 'door-open'
 TIMEOUT = 60  # seconds
+OPEN_SEQUENCE = 'ww999'
 
 
 @app.route('/knock', methods=['POST'])
@@ -34,9 +35,43 @@ def knock():
 def open_door():
     response = twiml.Response()
     if redis_client.get(DOOR_IS_OPEN) == 'true':
-        response.play(digits='ww999')
+        response.play(digits=OPEN_SEQUENCE)
+
     else:
-        response.say(u'Sorry, the door is locked.', voice='alice')
+        with response.gather(action=u'/digits', finishOnKey=u'#') as g:
+            g.say(u'Please enter the passcode, followed by the pound key.',
+                  voice=u'alice')
+            g.pause(length=5)
+
+        response.say(
+            u"Sorry, you didn't enter the passcode in time. Pleas try again.",
+            voice=u'alice',
+        )
+
+    response.hangup()
+    return unicode(response)
+
+
+@app.route('/digits', methods=['POST'])
+def gather_digits():
+    response = twiml.Response()
+    digits = request.form['Digits']
+    if not digits:
+        response.say(
+            u"Sorry, you didn't enter the passcode in time. Please try again.",
+            voice=u'alice',
+        )
+
+    elif digits == SECRET_KEY:
+        response.say(u'Passcode accepted. Please enter.', voice=u'alice')
+        response.play(OPEN_SEQUENCE)
+
+    else:
+        response.say(
+            u"Sorry, that wasn't the right passcode. Please try again.",
+            voice=u'alice',
+        )
+
     response.hangup()
     return unicode(response)
 
